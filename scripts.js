abdbe917d6d4a2373b9279c52f2a86637db14c82
Kherlysson Ryann <ryann.gto@gmail.com>
2026-08-18 20:07:51 -0300

    feat: Melhorar a leitura de arquivos e exibição de imagem no upload

--- scripts.js
+++ scripts.js
@@ -19,3 +19,21 @@ function lerConteudoDoArquivo(arquivo) {
         leitor.readAsDataURL(arquivo)
     })
 }
+
+const imagemPrincipal = document.querySelector(".main-imagem")
+const nomeDaImagem = document.querySelector(".container-imagem-nome p")
+
+inputUpload.addEventListener("change", async (evento) => {
+    const arquivo = evento.target.files[0]
+
+    if(arquivo){
+        try{
+            const conteudoDoArquivo = await lerConteudoDoArquivo(arquivo);
+            imagemPrincipal.src = conteudoDoArquivo.url
+            nomeDaImagem.textContent = conteudoDoArquivo.nome
+        } catch (erro){
+            console.erro("Erro na leitura do arquivo")
+        }
+    }
+})
+
